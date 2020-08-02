7bbeee5b3ec9005a3fdb83d007d0ffd69f36efd6
Bas_Peerawat <basza604@hotmail.com>
2020-08-02 17:26:43 +0700

    intitial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let iNumber = document.getElementById('in-num') 
+let show = document.getElementById('show')
+
+
+function msg(){
+    console.log(typeof(iNumber));
+    let IINumber = Number(iNumber.value)
+    console.log(typeof(IINumber));
+    let result = '';
+    for(let i = 1 ; i<= 12 ; i++){
+        result += '<p>'
+        result +=  (IINumber * i)
+        result += '</p>'
+
+    }
+    show.innerHTML = result
+    console.log(result);
+}
+    
+    
+
+
+
+
+
+
+// for(let i = 1; i <= 12 ;i++){
+//     if ( i % 2 === 0 ){
+//         continue;
+//     }
+//     if( i % 3 == 0){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// let content1  = document.getElementById('content-1');
+// let textHtml = '<b>เกิดมา</b>'
+// textHtml += '<i>กฟหกฟ/</i>'
+// content1.innerHTML = textHtml
+
